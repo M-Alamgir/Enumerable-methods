d1e626cbfa0c2da5a9bc56f2c431ef1c2d42940b
patrick <angelosp07@gmail.com>
2021-01-06 15:16:52 +0200

    Add my each_with_index, select, all, any and none methods

--- methods.rb
+++ methods.rb
@@ -26,6 +26,53 @@ module Enumerable
         return temp
     end
 
+    def my_each_with_index
+        i = 0
+        (self.length).times do
+            yield(self[i], i)
+            i += 1
+        end
+        return self
+    end
+
+    def my_select
+        temp = []
+        i = 0
+        (self.length).times do
+            if (yield(self[i]))
+                temp.push(self[i])
+            end
+            i += 1
+        end
+        return temp
+    end
+
+    def my_all?
+        i = 0
+        (self.length).times do
+            return false unless (yield(self[i]))
+            i += 1
+        end
+        return true
+    end
+
+    def my_any?
+        i = 0
+        (self.length).times do
+            return true if (yield(self[i]))
+            i += 1
+        end
+        return false
+    end
+
+    def my_none?
+        i = 0
+        (self.length).times do
+            return false if (yield(self[i]))
+            i += 1
+        end
+        return true
+    end
 end
 
 a = [2, 4, 6, 7, 8, 10, 10]
