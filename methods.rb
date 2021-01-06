e49baa69c6bfd456ade571ad7ffe6f3d8e68e9c7
Alamgir <ourandoc@gmail.com>
2021-01-06 18:19:01 +0500

    Enumerables project complete.

--- methods.rb
+++ methods.rb
@@ -73,6 +73,45 @@ module Enumerable
         end
         return true
     end
+
+    def my_count(arg = nil)
+        if arg != nil
+          i = 0
+          count = 0
+          (self.length).times do
+              count += 1 if self[i] == arg
+              i += 1
+          end
+          return count
+        end
+        return self.length
+    end
+
+    def my_inject(arg = nil)
+        accumulator = arg
+        if arg == nil
+          accumulator = self[0]
+          i = 1
+          (self.length - 1).times do
+            accumulator = yield(accumulator, self[i])
+            i += 1
+          end
+        else
+          i = 0
+          (self.length).times do
+            accumulator = yield(accumulator, self[i])
+            i += 1
+          end
+        end
+
+        return accumulator
+    end
+end
+
+def multiply_els(arr)
+    arr.my_inject { |multi, num| multi *= num}
 end
 
 a = [2, 4, 6, 7, 8, 10, 10]
+map_proc = Proc.new {|num| num += 1}
+p a.my_map(map_proc) {|num| num = 1}
